e05a2aef9db17c772a093afa1d061e22cf2351be
Shivam Singh Rajput <shivamkaushik26t@gmail.com>
2026-03-26 14:57:10 +0530

    "api service and email verification"

--- perplexity/src/routes/auth.routes.js
+++ perplexity/src/routes/auth.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import { register } from "../controllers/auth.controller.js";
-import { registerValidator } from "../validators/auth.validator.js";
-
+import { register,verifyEmail,login,getMe } from "../controllers/auth.controller.js";
+import { registerValidator,loginValidator } from "../validators/auth.validator.js";
+import { authUser } from "../middlewares/auth.middleware.js";
 const authRouter = Router();
 
 /**
@@ -11,5 +11,19 @@ const authRouter = Router();
  * @body { username, email, password }
  */
 authRouter.post("/register", registerValidator, register);
+/** * @route POST /api/auth/login
+ * @desc Login user and return JWT token
+ * @access Public
+ * @body { email, password }
+ */
+authRouter.post("/login", loginValidator, login);
 
+
+/** * @route GET /api/auth/verify-email?token=...
+ * @desc Verify user's email address
+ * @access Public
+ * @query { token }
+ */ 
+authRouter.get("/get-me", authUser, getMe);
+authRouter.get("/verify-email", verifyEmail);
 export default authRouter;
