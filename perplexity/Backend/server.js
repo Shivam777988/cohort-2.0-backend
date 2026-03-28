7929046dd30066cd3740fecdfa60c5a26aec01f9
Shivam Singh Rajput <shivamkaushik26t@gmail.com>
2026-03-28 15:50:48 +0530

    "with redux frontend of perplexity"

--- perplexity/Backend/server.js
+++ perplexity/Backend/server.js
@@ -3,7 +3,7 @@ import app from "./src/app.js";
 import connectDB from "./src/config/database.js";
 import { testAi } from "./src/services/ai.service.js";
 const PORT = process.env.PORT || 8000;
-testAi();
+// testAi();
 connectDB()
     .catch((err) => {
         console.error("MongoDB connection failed:", err);
