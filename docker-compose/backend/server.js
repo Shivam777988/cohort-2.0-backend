f06cbfd52ff95f957959d20c57e75309a279e0c3
Shivam Singh Rajput <shivamkaushik26t@gmail.com>
2026-05-11 15:34:39 +0530

    "DOCKER COMPOSE for fd and bd"

---
+++ docker-compose/backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+const app = express();
+app.use(morgan('dev'));
+
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ status: "OK" });
+});
+
+app.get("/api/hello", (req, res) => {
+    res.status(200).json({ message: "Hello, World!" });
+});
+
+app.get("/api/users", (req, res) => {
+    const users = [
+        { id: 1, name: "Alice" },
+        { id: 2, name: "Bob" },
+        { id: 3, name: "Charlie" },
+      
+    ];
+    res.status(200).json(users);
+})
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
